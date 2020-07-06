305ed572240958a17d23c9547f73527328dbde93
ThilakSowmiya1 <67905099+ThilakSowmiya1@users.noreply.github.com>
2020-07-06 19:49:22 +0530

    Add files via upload

---
+++ my_script (3).sh
@@ -0,0 +1,7 @@
+echo -e "\e[36mHappy Birthday 🎂 Panda 🐼 Kutty\e[0m";
+echo -e "HAPPY BIRTHDAY 🎉🎈🎈 PANDA 🐼 KUTTY";
+echo -e "\e[31mHAPPY BIRTHDAY 🎂🎉🎉🎉🎉🎂 PANDA 🐼 KUTTY\e[0m";
+if [ "${@:-}" = "count=0 while [ $count -le 10 ] do echo "$count" count=$(( $count + 1 )) done" ]
+then
+	echo -e "\e[92mHappy Birthday 🎂🎂🎉🎈🎈 Panda 🐼 Kutty\e[0m";
+fi
